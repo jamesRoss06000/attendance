e4fc7a24e8c1403b58bcb3e62a18ad093f5e0906
ROSS James <j.ross@ics-nice.com>
2019-08-09 11:07:02 +0200

    test

--- myApp/src/app/api/getStudentNames.php
+++ myApp/src/app/api/getStudentNames.php
@@ -1,7 +1,7 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Origin, Content-Type");
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Headers: Origin, Content-Type");
 
 // require_once("connection2.php");
 
