2e7ef127e4fb4a0200273ea8b8e88a711073f819
ROSS James <j.ross@ics-nice.com>
2019-08-09 11:04:29 +0200

    test

--- myApp/src/app/api/getStudentNames.php
+++ myApp/src/app/api/getStudentNames.php
@@ -7,7 +7,7 @@ require_once("connection2.php");
 
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
-
+echo json_encode("test");
 if (isset($_POST["date"])) {
 
     $origDate = date("Y-m-d", strtotime($_POST['date']));
