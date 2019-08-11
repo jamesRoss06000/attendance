b1273e0b43981c695c9aa69ecade574b14cff7d7
ROSS James <j.ross@ics-nice.com>
2019-08-11 19:58:15 +0200

    test

--- myApp/src/app/api/getStudentNames.php
+++ myApp/src/app/api/getStudentNames.php
@@ -19,23 +19,23 @@ if (isset($_POST["date"])) {
     $getClasse->execute([':id_planning' => $id_planning]);
     $details = $getClasse->fetchAll();
     $classe = $details[0]['classe'];
-    echo $classe;
+    // echo $classe;
+    if (isset($classe)) {
+        $stmt = $conn->prepare("SELECT * FROM users WHERE `classe` = :classe");
+        $stmt->execute([':classe' => $classe]);
 
-    // $stmt = $conn->prepare("SELECT * FROM users WHERE `classe` = :classe");
-    // $stmt->execute([':classe' => $classe]);
-    
-    // if ($stmt->rowCount() > 0) {
-    //     $output = array();
-    //     $output = $stmt->fetchAll();
-    //     $newOutput = array();
-    //     array_push($newOutput, $output, $id_planning);
-    //     echo json_encode($newOutput);
-    // } else {
-    //     $errors = "No data found for this date!";
-    //     echo json_encode($errors);
-    // }
+        if ($stmt->rowCount() > 0) {
+            $output = array();
+            $output = $stmt->fetchAll();
+            $newOutput = array();
+            array_push($newOutput, $output, $id_planning);
+            echo json_encode($newOutput);
+        } else {
+            $errors = "No data found for this date!";
+            echo json_encode($errors);
+        }
+    }
     // $conn->close();
-}
-else{
+} else {
     echo "get";
 }
