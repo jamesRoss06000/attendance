488125d74402d296b512b3b4e088b7ac621771e1
ROSS James <j.ross@ics-nice.com>
2019-08-12 18:03:04 +0200

    trying without classe

--- myApp/src/app/api/getStudentNames.php
+++ myApp/src/app/api/getStudentNames.php
@@ -17,8 +17,8 @@ if (isset($_POST["date"])) {
     $classe = $details[0]['classe'];
     $nom = $details[0]['nom'];
 
-    $stmt = $conn->prepare("SELECT * FROM users WHERE `nom` = :nom AND `classe` = :classe");
-    $stmt->execute([':nom' => $nom, ':classe' => $classe]);
+    $stmt = $conn->prepare("SELECT * FROM users WHERE `nom` = :nom");
+    $stmt->execute([':nom' => $nom]);
     if ($stmt->rowCount() > 0) {
         $output = array();
         $output = $stmt->fetchAll();
