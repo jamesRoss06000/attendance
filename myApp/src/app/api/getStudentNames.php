3456dc77421ee80541cd167646abb6ddd51e15fc
ROSS James <j.ross@ics-nice.com>
2019-08-12 13:37:47 +0200

    getNamesIsFucked

--- myApp/src/app/api/getStudentNames.php
+++ myApp/src/app/api/getStudentNames.php
@@ -1,38 +1,34 @@
 <?php
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, Content-Type");
-
 require_once("connection2.php");
-
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
-
-if (isset($_POST['date'])) {
-
+//var_dump($_POST);
+if (isset($_POST["date"])) {
     $origDate = date("Y-m-d", strtotime($_POST['date']));
     $date = $origDate;
     $id = $_GET['id'];
     $id_planning = $_POST['id_planning'];
-    // echo json_encode($date);
-    $getClasse = $conn->prepare("SELECT * FROM planning WHERE `id_planning` = :id_planning");
+    $getClasse = $conn->prepare("SELECT classe FROM planning WHERE id_planning = :id_planning");
     $getClasse->execute([':id_planning' => $id_planning]);
-    $getDetails = $getClasse->fetchAll();
-    $classe = $getDetails[0]['classe'];
-
-    $stmt = $conn->prepare("SELECT * FROM users WHERE `classe` = classe OR `nom` = :nom");
-    $stmt->execute([':nom' => $classe, ':classe' => $classe]);
-
+    $details = $getClasse->fetchAll();
+    $classe = $details[0]['classe'];
+    // var_dump($classe);
+    $stmt = $conn->prepare("SELECT * FROM users WHERE `classe` = :classe OR `nom` = :nom");
+    $stmt->execute([':classe' => $classe, ':nom' => $classe]);
     if ($stmt->rowCount() > 0) {
         $output = array();
         $output = $stmt->fetchAll();
-        $newArray = array();
-        array_push($newArray, $output, $id_planning);
-        echo json_encode($output);
+        $newOutput = array();
+        array_push($newOutput, $output, $id_planning);
+        echo json_encode($newOutput);
     } else {
-        $errors = "No data found for this date";
+        $errors = "No data found for this date!";
         echo json_encode($errors);
-        return;
     }
     // $conn->close();
 }
+// else{
+//     echo "get";
+// }
