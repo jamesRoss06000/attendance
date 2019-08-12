5d2e0bdacf3e4f1fd7c9177116f739ddfe5a46ff
ROSS James <j.ross@ics-nice.com>
2019-08-12 13:36:28 +0200

    tests

--- myApp/src/app/api/getStudentNames.php
+++ myApp/src/app/api/getStudentNames.php
@@ -8,24 +8,26 @@ require_once("connection2.php");
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
-if (isset($_POST["date"])) {
+if (isset($_POST['date'])) {
 
     $origDate = date("Y-m-d", strtotime($_POST['date']));
     $date = $origDate;
     $id = $_GET['id'];
+    $id_planning = $_POST['id_planning'];
+    // echo json_encode($date);
+    $getClasse = $conn->prepare("SELECT * FROM planning WHERE `id_planning` = :id_planning");
+    $getClasse->execute([':id_planning' => $id_planning]);
+    $getDetails = $getClasse->fetchAll();
+    $classe = $getDetails[0]['classe'];
 
-    $getStudent = $conn->prepare("SELECT * FROM users WHERE id = :id");
-    $getStudent->execute([':id' => $id]);
-    $studentDetails = $getStudent->fetchAll();
-    $studentClasse = $studentDetails[0]['classe'];
-    $studentNom = $studentDetails[0]['nom'];
-
-    $stmt = $conn->prepare("SELECT * FROM planning WHERE `classe` = (:studentClasse OR :studentNom) AND `date` = :date ORDER BY `debut_am` ASC");
-    $stmt->execute([':date' => $date, ':studentClasse' => $studentClasse, ':studentNom' => $studentNom]);
+    $stmt = $conn->prepare("SELECT * FROM users WHERE `classe` = classe OR `nom` = :nom");
+    $stmt->execute([':nom' => $classe, ':classe' => $classe]);
 
     if ($stmt->rowCount() > 0) {
         $output = array();
         $output = $stmt->fetchAll();
+        $newArray = array();
+        array_push($newArray, $output, $id_planning);
         echo json_encode($output);
     } else {
         $errors = "No data found for this date";
