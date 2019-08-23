8246238769568b2cb2ba51687b66b23662e35603
ROSS James <j.ross@ics-nice.com>
2019-08-23 15:59:07 +0200

    test

--- addPlanningTreatment.php
+++ addPlanningTreatment.php
@@ -55,31 +55,31 @@ if (isset($_POST["date"], $_POST["lieux"], $_POST["cours"])) {
     if (empty($errors)) {
         $classe = filter_input(INPUT_POST, "classe");
         $date = filter_input(INPUT_POST, "date");
-        $lieux = filter_input(INPUT_POST, "lieux");
-        $getAdresse = $conn->prepare("SELECT * FROM lieux WHERE campus = :lieux");
-        $getAdresse->execute([':lieux' => $lieux]);
-        $adresseList = $getAdresse->fetchAll();
-        $adresse = $adresseList[0]['adresse'];
+            $lieux = filter_input(INPUT_POST, "lieux");
+            $getAdresse = $conn->prepare("SELECT * FROM lieux WHERE campus = :lieux");
+            $getAdresse->execute([':lieux' => $lieux]);
+            $adresseList = $getAdresse->fetchAll();
+            $adresse = $adresseList[0]['adresse'];
         $cours = filter_input(INPUT_POST, "cours");
         $theme = filter_input(INPUT_POST, "theme");
         $debut_am = filter_input(INPUT_POST, "debut_am");
         $fin_am = filter_input(INPUT_POST, "fin_am");
         $debut_pm = filter_input(INPUT_POST, "debut_pm");
         $fin_pm = filter_input(INPUT_POST, "fin_pm");
-        $intervenant_id = filter_input(INPUT_POST, "intervenant");
-        $getId = $conn->prepare("SELECT * FROM users WHERE id = :intervenant_id");
-        $getId->execute([':intervenant_id' => $intervenant_id]);
-        $details = $getId->fetchAll();
-        $intervenant_name = $details[0]['nom'];
+            $intervenant_id = $_GET['id'];
+            $getTeacher = $conn->prepare("SELECT * FROM users WHERE id = :intervenant_id");
+            $getTeacher->execute([':intervenant_id' => $intervenant_id]);
+            $intervenantList = $getTeacher->fetchAll();
+            $intervenant_name = $intervenantList[0]['nom'];
 
         if (!$conn) {
             echo "Error: Unable to connect to MySQL." . PHP_EOL;
             echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
             echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
         } else {
-            $sql = $conn->prepare("INSERT INTO `planning`(`id_planning`, `date`,  `lieux`, `adresse`, `cours`, `theme`, `debut_am`, `fin_am`, `debut_pm`, `fin_pm`, `intervenant_name`, `intervenant_id`, `classe`, `nom`) VALUES (NULL, :date, :lieux, :adresse, :cours, :theme, :debut_am, :fin_am, :debut_pm, :fin_pm, :intervenant_name, :intervenant_id, :classe, NULL)");
-            $sql->execute([':date' => $date, ':lieux' => $lieux, ':adresse' => $adresse, ':cours' => $cours, ':theme' => $theme, ':debut_am' => $debut_am, ':fin_am' => $fin_am, ':debut_pm' => $debut_pm, ':fin_pm' => $fin_pm, ':intervenant_name' => $intervenant_name, ':intervenant_id' => $intervenant_id, ':classe' => $classe]);
-            // header('Location: addPlanning.php?id=Database updated');
+            $sql = $conn->prepare("INSERT INTO `planning`(`id_planning`, `date`, `lieux`, `adresse`, `cours`, `theme`, `debut_am`, `fin_am`,`intervenant_name`, `intervenant_id`, `classe`, `nom`) VALUES (NULL, :date, :lieux, :adresse, :cours, :theme, :time, :timeEnd, :intervenant_name, :intervenant_id, :classe, :name)");
+            $sql->execute([':date' => $date, ':lieux' => $lieux, ':adresse' => $adresse, ':cours' => $cours, ':theme' => $theme, ':time' => $time, ':timeEnd' => $timeEnd, ':intervenant_name' => $intervenant_name, 'intervenant_id' => $intervenant_id, ':classe' => $classe, ':name' => $name]);
+            header('Location: addPlanning.php?id=Database updated');
         }
         // $conn->close();
     }
