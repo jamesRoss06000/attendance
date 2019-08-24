6b99728059dde1c5b6fdd3a0624e417c42edd072
ROSS James <j.ross@ics-nice.com>
2019-08-24 11:22:14 +0200

    edit

--- modifyUser.php
+++ modifyUser.php
@@ -1,6 +1,6 @@
 <?php
 require_once("connection.php");
-require_once("modifyAbsencesTreatment.php");
+require_once("modifyUserTreatment.php");
 
 if (!empty($_POST)) {
     $absenceId = $_GET['id'];
