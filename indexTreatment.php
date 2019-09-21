ef75830b0d9219fc758584f9ea1a0ef44778482c
ROSS James <j.ross@ics-nice.com>
2019-09-21 17:21:54 +0200

    tested

--- indexTreatment.php
+++ indexTreatment.php
@@ -6,17 +6,27 @@ if (isset($_POST["email"], $_POST["password"])) {
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-    $sql = $conn->prepare("SELECT * FROM users WHERE email = :email AND password = :password");
-    $sql->bindParam(':email', $email);
-    $sql->bindParam(':password', $password);
-    $sql->execute();
+    $sql = "SELECT email, password, role FROM users WHERE email = '" . $email . "' AND password = '" . $password . "'";
+    $result1 = $conn->query($sql);
     if ($result1->rowCount() > 0 && $email == "admin@admin.com") {
         $LoggedIn = true;
-        sleep(2);
         session_start();
         $_SESSION['admin'] = "Admin logged In";
+        sleep(2);
         header("Location: mainMenu.php");
     } else {
         $errors = "The username or password are incorrect!";
     }
 }
+
+
+// $sql = $conn->prepare("SELECT * FROM users WHERE email = :email AND password = :password");
+//     $sql->bindParam(':email', $email);
+//     $sql->bindParam(':password', $password);
+//     $sql->execute();
+//     if ($result1->rowCount() > 0 && $email == "admin@admin.com") {
+//         $LoggedIn = true;
+//         sleep(2);
+//         session_start();
+//         $_SESSION['admin'] = "Admin logged In";
+//         header("Location: mainMenu.php");
