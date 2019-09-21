adefc96c689272b9a1aed420db848c1c6c0cc160
ROSS James <j.ross@ics-nice.com>
2019-09-21 18:20:20 +0200

    test

--- addEtudiantToCours.php
+++ addEtudiantToCours.php
@@ -140,7 +140,9 @@ session_start();
     </div>
     <?php
     if (!isset($_SESSION['admin'])) {
-        require_once("ifSessionNotSet.php");
+        echo "<b>Please login to use this system</b>";
+        echo "<td><a class='btn btn-danger btn-modal btn-md' id='login' href='index.php'>Click To Login</a></td>";
+        echo "<script>$(':button').prop('disabled', true);</script>";
     }
     ?>
     <script>
