1c23295fcf4a1708679a68633dbca494e509d1b7
ROSS James <j.ross@ics-nice.com>
2019-09-23 11:19:09 +0200

    delete $num

--- addCoursPourClasseTreatment.php
+++ addCoursPourClasseTreatment.php
@@ -42,7 +42,7 @@ if (isset($_POST["classe"])) {
         $row = 1;
         if (($handle = fopen($_FILES["fileUpload"]["tmp_name"], "r")) !== FALSE) {
             while (($data = fgetcsv($handle, 0, ";")) !== FALSE) {
-                $num = count($data);
+                // $num = count($data);
                 // print_r($data);
                 if ($row > 1) {
                     $dateFile = explode("/", $data[0]);
@@ -67,8 +67,15 @@ if (isset($_POST["classe"])) {
                     $nom = "";
 
                     if (strlen($intervenant_name) > 1) {
-                        $sql = $conn->prepare("INSERT INTO `planning`(`id_planning`, `date`, `lieux`, `adresse`, `cours`, `theme`, `debut_am`, `fin_am`, `debut_pm`, `fin_pm`, `intervenant_name`, `intervenant_id`, `classe`, `nom`) VALUES (NULL, :date, :lieux, :adresse, :cours, :theme, :debut_am, :fin_am, :debut_pm, :fin_pm, :intervenant_name, :intervenant_id, :classe, :nom)");
-                        $sql->execute([':date' => $date, ':lieux' => $lieux, ':adresse' => $adresse, ':cours' => $cours, ':theme' => $theme, ':debut_am' => $debut_am, ':fin_am' => $fin_am, ':debut_pm' => $debut_pm, ':fin_pm' => $fin_pm, ':intervenant_name' => $intervenant_name, 'intervenant_id' => $intervenant_id, ':classe' => $classe, ':nom' => $nom]);
+                        $sql = $conn->prepare("INSERT INTO `planning`(`id_planning`, `date`, `lieux`,
+                        `adresse`, `cours`, `theme`, `debut_am`, `fin_am`, `debut_pm`, `fin_pm`,
+                        `intervenant_name`, `intervenant_id`, `classe`, `nom`) VALUES (NULL, :date,
+                        :lieux, :adresse, :cours, :theme, :debut_am, :fin_am, :debut_pm, :fin_pm, :intervenant_name,
+                        :intervenant_id, :classe, :nom)");
+                        $sql->execute([':date' => $date, ':lieux' => $lieux, ':adresse' => $adresse,
+                        ':cours' => $cours, ':theme' => $theme, ':debut_am' => $debut_am, ':fin_am' => $fin_am,
+                        ':debut_pm' => $debut_pm, ':fin_pm' => $fin_pm, ':intervenant_name' => $intervenant_name,
+                        'intervenant_id' => $intervenant_id, ':classe' => $classe, ':nom' => $nom]);
                     }
                 }
                 $row++;
