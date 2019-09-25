a3309877ee246f4ed64a42f523afe288053516c5
ROSS James <j.ross@ics-nice.com>
2019-09-25 11:33:48 +0200

    without http

--- myApp/src/app/login/login.page.ts
+++ myApp/src/app/login/login.page.ts
@@ -1,29 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// Voir ci-dessus : Les composants et modules Angular sont 
+// importés sur chaque page dont nous avons besoin.
 import { AuthService } from '../services/auth.service';
-import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-
+// On importe le fichier de service qu'on a créé (nous verrons cela plus
+// en détail bientôt). Il s'agit d'un fichier qui a des fonctions et 
+// sert également de lien entre les différentes pages de l'application.
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+// En haut : Détails des composants tels que le sélecteur et les 
+// chemins de fichiers pour les fichiers HTML et CSS.
+
+// Ci-dessous : Une classe est créée automatiquement dans le fichier.ts de chaque composant. 
 export class LoginPage implements OnInit {
-
-  // authState$: Observable<boolean>;
-
-  constructor(private Auth: AuthService, public http: HttpClient) { }
+  // On place toutes les dépendances dans le constructeur, qui est appelé sur l'initialisation du composant.
+  constructor(private Auth: AuthService) { }
 
   ngOnInit() {
   }
-
+  // Ci-dessous se trouve la fonction que l'utilisateur appelle à partir de la page HTML, 
+  // lorsqu'il soumet les détails de l'utilisateur.
   loginUser(event){
     event.preventDefault();
     const target = event.target;
     const email = target.querySelector('#email').value;
     const password = target.querySelector('#password').value;
-
+// On met les deux valeurs dans des variables qui sont passées à la page de service'auth.service.ts'
+//  comme arguments dans la fonction getUserDetails()
     this.Auth.getUserDetails(email, password);
-    console.log(email, password, "test");
+    // console.log(email, password, "test");
   }
 }
