937ff2f732597d3c164fd9df4c6358f635f80a78
ROSS James <j.ross@ics-nice.com>
2019-09-26 14:31:22 +0200

    test without event declared

--- myApp/src/app/teacherdate/teacherdate.page.ts
+++ myApp/src/app/teacherdate/teacherdate.page.ts
@@ -14,11 +14,12 @@ import { AlertController } from '@ionic/angular';
 export class TeacherdatePage {
   planning: any;
 
-  constructor(private Auth: AuthService, public http: HttpClient, private router: Router, private route: ActivatedRoute, private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string) {
+  constructor(private Auth: AuthService, public http: HttpClient, private router: Router, private route: ActivatedRoute,
+     private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string) {
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.planning = this.router.getCurrentNavigation().extras.state.planning;
-        console.log(this.planning);
+        // On extrait les données de l'objet à l'aide de element=>.
         this.planning.forEach(element => {
           let dateEvent = [];
           if (element.debut_am != "") {
@@ -27,9 +28,15 @@ export class TeacherdatePage {
           if (element.debut_pm != "") {
             dateEvent.push([element.debut_pm, element.fin_pm]);
           }
+          // ^^ Les heures de début et de fin de chaque ligne (chaque leçon dans la planification) 
+          // sont poussées dans le tableau dateEvent.
+
+          // 
           dateEvent.forEach(d => {
+          // Les heures de début et de fin sont ajoutées à la date.
             let dateStartEvent = element.date + " " + d[0];
             let dateEndEvent = element.date + " " + d[1];
+          // 'eventTmp' est rempli de valeurs d'élément
             let eventTmp = {
               title: element.cours,
               desc: element.lieux + " " + element.adresse,
@@ -38,6 +45,7 @@ export class TeacherdatePage {
               allDay: false,
               id: element.id_planning
             }
+          // new event pushed into eventSource array
             this.eventSource.push(eventTmp);
           });
 
@@ -59,14 +67,14 @@ export class TeacherdatePage {
     currentDate: new Date(),
   }
 
-  event = {
-    title: "",
-    desc: '',
-    startTime: '',
-    endTime: "",
-    allDay: false,
-    classId: ""
-  };
+  // event = {
+  //   title: "",
+  //   desc: '',
+  //   startTime: '',
+  //   endTime: "",
+  //   allDay: false,
+  //   classId: ""
+  // };
 
   ngOnInit() {
     this.resetEvent();
@@ -83,28 +91,27 @@ export class TeacherdatePage {
     };
   }
 
-  // Change current month/week/day
+  // Modifier le mois/semaine/jour en cours
   next() {
     var swiper = document.querySelector('.swiper-container')['swiper'];
     swiper.slideNext();
   }
-
   back() {
     var swiper = document.querySelector('.swiper-container')['swiper'];
     swiper.slidePrev();
   }
 
-  // Focus today
+  // Focus aujourd'hui
   today() {
     this.calendar.currentDate = new Date();
   }
 
-  // Selected date reange and hence title changed
+  // Changement des dates sélectionnée et donc du titre
   onViewTitleChanged(title) {
     this.viewTitle = title;
   }
 
-  // Calendar event was clicked
+  // L'événement du calendrier a été cliqué
   async onEventSelected(event) {
     {
       let date = event.startTime;
@@ -115,13 +122,17 @@ export class TeacherdatePage {
     }
   }
 
+  // Même fonction sur chaque page (sauf login). Redirige l'utilisateur vers la 
+  // page précédente, avec l'ID ajouté à la fin de l'URL
   backPage(event) {
     event.preventDefault();
     const url = window.location.href;
     const id = url.substring(url.lastIndexOf('/') + 1);
     this.router.navigate(['/home/', id]);
   }
-
+  
+  // Même fonction sur chaque page (sauf login).Redirige l'utilisateur vers la 
+  // page '/login'.
   logout() {
     this.router.navigate(['/login/']);
   }
