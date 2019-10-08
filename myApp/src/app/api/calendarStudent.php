c65aac675e56cbbb840fc41d831b40061eb8d4ba
ROSS James <j.ross@ics-nice.com>
2019-10-08 13:00:15 +0200

    calendarStduentPHP sql changed

--- myApp/src/app/api/calendarStudent.php
+++ myApp/src/app/api/calendarStudent.php
@@ -17,7 +17,7 @@ if (isset($_POST["id"])) {
     $name = $userDetails[0]['nom'];
     $classe = $userDetails[0]['classe'];
 
-    $stmt = $conn->prepare("SELECT * FROM planning WHERE `nom` = :name OR `classe` = :classe");
+    $stmt = $conn->prepare("SELECT * FROM planning WHERE `nom` = :name OR (`classe` = :classe AND `nom` IS NULL)");
     $stmt->execute([':name' => $name, ':classe' => $classe]);
 
     if ($stmt->rowCount() > 0) {
