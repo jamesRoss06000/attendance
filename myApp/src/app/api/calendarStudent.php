4d72c0ff93c0d7a8d6edc04599035133bfdba012
ROSS James <j.ross@ics-nice.com>
2019-10-08 13:49:13 +0200

    startDate added TEST

--- myApp/src/app/api/calendarStudent.php
+++ myApp/src/app/api/calendarStudent.php
@@ -17,8 +17,15 @@ if (isset($_POST["id"])) {
     $name = $userDetails[0]['nom'];
     $classe = $userDetails[0]['classe'];
 
-    $stmt = $conn->prepare("SELECT * FROM planning WHERE `classe` = :classe OR `nom` = :name);
-    $stmt->execute([':name' => $name, ':classe' => $classe]);
+    $stmtC = $conn->prepare("SELECT * FROM classes WHERE `classe` = :classe");
+    $stmtC->execute([':classe' => $classe]);
+        $classOutput = array();
+        $classOutput = $stmtC->fetchAll();
+        $startDate = $classOutput[0]['debut_cours'];
+        $endDate = $classOutput[0]['fin_cours'];
+
+    $stmt = $conn->prepare("SELECT * FROM planning WHERE `classe` = :classe OR `nom` = :name AND [Date] BETWEEN ':startDate' AND ':endDate 23:59:59'");
+    $stmt->execute([':name' => $name, ':classe' => $classe, ':startDate' => $startDate, ':endDate' => $endDate]);
 
     if ($stmt->rowCount() > 0) {
         $output = array();
