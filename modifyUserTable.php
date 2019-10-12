c8adfeaa3f66c34f3039864380981dd3686b565e
James Ross <james_ross@outlook.fr>
2019-10-12 19:57:22 +0200

    $row id

--- modifyUserTable.php
+++ modifyUserTable.php
@@ -7,9 +7,10 @@ $request = $result->execute();
 
 echo "<table border='1' id='myTable'>";
 echo "<thead><tr><th>Id</th><th>Nom</th><th>Telephone</th><th>Email</th><th>Mot de pass</th><th>Role</th><th>Classe</th><th>Supprimez une ligne</th><th>Modifiez une ligne</th></tr></thead>\n";
-$i = 1;
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    if ($i < 1){
+    if ($row['id'] == 1){
+        break;}
+    else {
     echo "<tr>";
     foreach ($row as $key => $value) {
         echo "<td>$value</td>";
@@ -18,6 +19,5 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     echo "<td><a class='btn btn-warning btn-modal btn-md' href='modifyUser.php?id=".$row['id']."'>Modify</a></td>";
     echo "</tr>";
     }
-    $i++;
 }
 echo "</table>";
