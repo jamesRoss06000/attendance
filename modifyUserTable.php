cb11dd561dde257827c2801bb3acb25a543466c8
James Ross <james_ross@outlook.fr>
2019-10-12 19:59:02 +0200

    again

--- modifyUserTable.php
+++ modifyUserTable.php
@@ -9,8 +9,8 @@ echo "<table border='1' id='myTable'>";
 echo "<thead><tr><th>Id</th><th>Nom</th><th>Telephone</th><th>Email</th><th>Mot de pass</th><th>Role</th><th>Classe</th><th>Supprimez une ligne</th><th>Modifiez une ligne</th></tr></thead>\n";
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     if ($row['id'] == 1){
-        break;}
-    else {
+        return;
+    } else {
     echo "<tr>";
     foreach ($row as $key => $value) {
         echo "<td>$value</td>";
