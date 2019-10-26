3dd79bc9b73b75d92f96eb3a48f3925e4896201f
james ross <james_ross@outlook.fr>
2019-10-26 12:24:28 +0200

    revert back because problems with api files

--- modifyUserTable.php
+++ modifyUserTable.php
@@ -8,7 +8,6 @@ $request = $result->execute();
 echo "<table border='1' id='myTable'>";
 echo "<thead><tr><th>Id</th><th>Nom</th><th>Telephone</th><th>Email</th><th>Mot de pass</th><th>Role</th><th>Classe</th><th>Supprimez une ligne</th><th>Modifiez une ligne</th></tr></thead>\n";
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    if ($row['id'] > 2){
     echo "<tr>";
     foreach ($row as $key => $value) {
         echo "<td>$value</td>";
@@ -16,6 +15,5 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     echo "<td><a class='btn btn-danger btn-md' href='deleteUser.php?id=".$row['id']."'>Delete</a></td>";
     echo "<td><a class='btn btn-warning btn-modal btn-md' href='modifyUser.php?id=".$row['id']."'>Modify</a></td>";
     echo "</tr>";
-    }
 }
 echo "</table>";
