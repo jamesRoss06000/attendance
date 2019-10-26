51e3aa5ec21a2340077088c7c2f8df5c4dad3cdf
james ross <james_ross@outlook.fr>
2019-10-26 14:05:03 +0200

    added dates to query

--- myApp/src/app/api/getStudentNames.php
+++ myApp/src/app/api/getStudentNames.php
@@ -18,8 +18,14 @@ if (isset($_POST["date"])) {
     $nom = $details[0]['nom'];
     $lieux = $details[0]['lieux'];
 
-    $stmt = $conn->prepare("SELECT * FROM users WHERE `nom` = :nom");
-    $stmt->execute([':nom' => $nom]);
+    $getDates = $conn->prepare("SELECT debut_cours, fin_cours FROM classes WHERE `classe` = :classe");
+    $getDates->execute([':classe' => $classe]);
+    $resultDates = $getDates->fetchAll();
+    $debut_cours = $resultDates[0]['debut_cours'];
+    $fin_cours = $resultDates[0]['fin_cours'];
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE `nom` = :nom AND $date BETWEEN $debut_cours AND $fin_cours 23:59:00");
+    $stmt->execute([':nom' => $nom, ':date' => $date, ':debut_cours' => $debut_cours, ':fin_cours' => $fin_cours]);
     if ($stmt->rowCount() > 0) {
         $output = $stmt->fetchAll();
         $newOutput = array();
